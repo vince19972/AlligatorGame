528660684d9696e93a392a9aeadf57948a6ffcd0
vince19972 <vince19972@gmail.com>
2019-03-20 11:50:03 -0400

    [ViewController] add entry view basic elements

--- alligator-game/alligator-game/ViewController.swift
+++ alligator-game/alligator-game/ViewController.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        /*-- MARK: create subviews --*/
+        let CreateElement = ElementCreation()
+        
+        // input elements container
+        let inputContainer =  UIView()
+            self.view.addSubview(inputContainer)
+            inputContainer
+                .translatesAutoresizingMaskIntoConstraints = false
+            inputContainer
+                .centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+            inputContainer
+                .centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+            inputContainer
+                .heightAnchor.constraint(equalToConstant: self.view.frame.height).isActive = true
+            inputContainer
+                .widthAnchor.constraint(equalToConstant: self.view.frame.width).isActive = true
+        
+        // name input textField
+        let nameInput = CreateElement.textField(placehoderText: "please input your name")
+            inputContainer.addSubview(nameInput)
+            nameInput
+                .centerXAnchor.constraint(equalTo: inputContainer.centerXAnchor).isActive = true
+            nameInput
+                .centerYAnchor.constraint(
+                    equalTo: inputContainer.centerYAnchor,
+                    constant: nameInput.frame.size.height / 2
+                ).isActive = true
+        
+        // submit button
+        let submitButton = CreateElement.submitButton(buttonText: "submit")
+            inputContainer.addSubview(submitButton)
+            submitButton
+                .centerXAnchor.constraint(equalTo: inputContainer.centerXAnchor).isActive = true
+            submitButton
+                .topAnchor.constraint(
+                    equalTo: nameInput.bottomAnchor,
+                    constant: 12
+                ).isActive = true
     }
 
 
