7d167e53522df5f14388710c2378a8d247737625
vince19972 <vince19972@gmail.com>
2019-03-26 17:07:17 -0400

    :sparkles: [ElementCreation] image, bg, textField

--- alligator-game/alligator-game/Helpers/ElementCreation.swift
+++ alligator-game/alligator-game/Helpers/ElementCreation.swift
@@ -15,19 +15,41 @@ import Foundation
 import UIKit
 
 class ElementCreation {
+
+    //
+    /*-- background --*/
+    //
+    func backgroundImage(_ imageName: String) -> UIImageView {
+        let background = UIImageView(frame: UIScreen.main.bounds)
+        background.image = UIImage(named: imageName)
+        background.contentMode = UIView.ContentMode.scaleAspectFill
+        
+        return background
+    }
+    
+    //
+    /*-- image --*/
+    //
+    func imageAsset(_ imageName: String) -> UIImageView {
+        let image = UIImageView(image: UIImage(named: imageName))
+        image.contentMode = UIView.ContentMode.scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        
+        return image
+    }
     
     //
     /*-- textField --*/
     //
-    func textField (placehoderText: String) -> TextField {
+    func textField(placehoderText: String) -> TextField {
         let textField = TextField()
         
         textField.translatesAutoresizingMaskIntoConstraints = false
-        // textField.backgroundColor = UIColor.black
-        // textField.layer.borderColor = UIColor.orange.cgColor
+        textField.backgroundColor = UIColor.white
         textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 10
         textField.font = UIFont(name: "HelveticaNeue-Light", size: 14)
-        // textField.textColor = UIColor.orange
+        textField.textColor = UIColor.gray
         textField.attributedPlaceholder = NSAttributedString(
             string: placehoderText,
             attributes: [NSAttributedString.Key.foregroundColor: UIColor.black]
@@ -40,17 +62,22 @@ class ElementCreation {
     //
     /*-- submit button --*/
     //
-    func submitButton(buttonText: String) -> UIButton {
+    func submitButton(buttonText: String, buttonImage: String? = nil) -> UIButton {
         let button = UIButton()
         
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle(buttonText, for: .normal)
+        if buttonImage != nil {
+            button.setImage(UIImage(named: buttonImage!), for: .normal)
+            button.imageView?.contentMode = UIView.ContentMode.scaleAspectFit
+        } else {
+            button.setTitle(buttonText, for: .normal)
             button.titleLabel?.font = UIFont(name: "HelveticaNeue-Light", size: 14)
             button.setTitleColor(.black, for: .normal)
-        button.layer.borderColor = UIColor.black.cgColor
+            button.layer.borderColor = UIColor.black.cgColor
             button.layer.borderWidth = 1
-        button.contentHorizontalAlignment = UIControl.ContentHorizontalAlignment.center
+            button.contentHorizontalAlignment = UIControl.ContentHorizontalAlignment.center
             button.contentEdgeInsets = UIEdgeInsets(top: 4, left: 4, bottom: 4, right: 4)
+        }
         
         return button
     }
