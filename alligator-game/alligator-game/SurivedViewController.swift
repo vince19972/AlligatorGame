33830108169ba2922133bc37a088e07f932bccc2
Annaooh <ao1631@nyu.edu>
2019-03-27 16:11:30 -0400

    endingview_edit

---
+++ alligator-game/alligator-game/SurivedViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SurivedViewController.swift
+//  alligator-game
+//
+//  Created by Anna Oh on 27/3/2019.
+//  Copyright © 2019 vinceshao. All rights reserved.
+//
+
+import UIKit
+
+class SurivedViewController: UIViewController {
+    @IBOutlet weak var survivedBackgroud: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
