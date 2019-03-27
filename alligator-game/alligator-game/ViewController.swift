6bb24a4c593fa75c13c7d5f5b1ac6b2f810ea7fc
Annaooh <ao1631@nyu.edu>
2019-03-27 17:05:27 -0400

    edning-view

--- alligator-game/alligator-game/ViewController.swift
+++ alligator-game/alligator-game/ViewController.swift
@@ -10,48 +10,54 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var deadBackground: UIImageView!
-    @IBOutlet weak var deadAlligator: UIImageView!
-    @IBOutlet weak var deadSign: UIImageView!
-    @IBOutlet weak var tryagainButton: UIButton!
-    @IBOutlet weak var exitButton: UIButton!
-    @IBOutlet weak var deadblood: UIImageView!
-    var player: AVAudioPlayer?
-
+    //background Image
+    let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+    backgroundImage.image = UIImage(named: "deadBackground")
+    backgroundImage.contentMode = .scaleAspectFill
+    self.view.insertSubview(backgroundImage, at: 0)
     
+//    @IBOutlet weak var deadBackground: UIImageView!
+//    @IBOutlet weak var deadAlligator: UIImageView!
+//    @IBOutlet weak var deadSign: UIImageView!
+//    @IBOutlet weak var tryagainButton: UIButton!
+//    @IBOutlet weak var exitButton: UIButton!
+//    @IBOutlet weak var deadblood: UIImageView!
+//    var player: AVAudioPlayer?
+//
+//
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    // Make the device vibrate.
-    func generateImpactFeedback() {
-        let generator = UIImpactFeedbackGenerator(style: .heavy)
-        generator.impactOccurred()
-    }
-    
-    // Play a mp3 sound file.
-    func playSoundMP3(filename: String) {
-        guard let url = Bundle.main.url(forResource: filename, withExtension: "mp3") else { return }
-        
-        do {
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category(rawValue: convertFromAVAudioSessionCategory(AVAudioSession.Category.playback)), mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-            
-            guard let player = player else { return }
-            player.play()
-        } catch let error {
-            print(error.localizedDescription)
-        }
-    }
-}
-
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertFromAVAudioSessionCategory(_ input: AVAudioSession.Category) -> String {
-    return input.rawValue
-}
+//    // Make the device vibrate.
+//    func generateImpactFeedback() {
+//        let generator = UIImpactFeedbackGenerator(style: .heavy)
+//        generator.impactOccurred()
+//    }
 //
-
+//    // Play a mp3 sound file.
+//    func playSoundMP3(filename: String) {
+//        guard let url = Bundle.main.url(forResource: filename, withExtension: "mp3") else { return }
+//
+//        do {
+//            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category(rawValue: convertFromAVAudioSessionCategory(AVAudioSession.Category.playback)), mode: .default)
+//            try AVAudioSession.sharedInstance().setActive(true)
+//
+//            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+//
+//            guard let player = player else { return }
+//            player.play()
+//        } catch let error {
+//            print(error.localizedDescription)
+//        }
+//    }
+//}
+//
+//
+//// Helper function inserted by Swift 4.2 migrator.
+//fileprivate func convertFromAVAudioSessionCategory(_ input: AVAudioSession.Category) -> String {
+//    return input.rawValue
+//}
+////
+//
+}
